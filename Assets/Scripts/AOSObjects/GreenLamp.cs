50c44bf91b79e7ad82f931fcc42920bc2e288e23
Alexander <sherstoboev93@gmail.com>
2023-03-30 16:59:28 +0300

    Logic lamp added
    -game object
    - script

---
+++ Assets/Scripts/AOSObjects/GreenLamp.cs
@@ -0,0 +1,18 @@
+using AosSdk.Core.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[AosSdk.Core.Utils.AosObject(name: "Lamp")]
+
+public class GreenLamp : SceneAosObject
+{
+    [SerializeField] private GameObject _greenLamp;
+    [AosAction(name: "Лампа")]
+    public void EnableLamp(bool value)
+    {
+       _greenLamp.SetActive(value);
+
+    }
+
+
+}
